7f9e61d7af149516ed4d2bf802f08ed2968d3deb
narayanan1994 <nanu.s94@gmail.com>
2024-08-13 21:51:38 +0530

    library project creation

---
+++ projects/test-ng-lib/src/lib/test-ng-lib.const.ts
@@ -0,0 +1,19 @@
+export interface UserList {
+    firstName: string;
+    lastName: string;
+}
+
+export const AllUsers: UserList[] = [
+    {
+        firstName: 'Narayanan',
+        lastName: 'Srinivasan'
+    },
+    {
+        firstName: 'Shiva',
+        lastName: 'Shankar'
+    },
+    {
+        firstName: 'Shivaram',
+        lastName: 'L.S'
+    }
+]
